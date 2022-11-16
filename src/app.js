7d1f0ede903e51b6c8b6f1bd9298b65b896d5c84
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2022-11-16 13:11:35 +0000

    excuse geneerator project. Ringo Starr excuses everytime

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here - variabels with excuses elements
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car", "my bike"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuseP = document.getElementById("excuse");
+
+  function excuseElements(excusesArr) {
+    let randomIndex = Math.floor(Math.random() * excusesArr.length);
+    return excusesArr[randomIndex];
+  }
+
+  let newExcuse =
+    excuseElements(who) +
+    " " +
+    excuseElements(action) +
+    " " +
+    excuseElements(what) +
+    " " +
+    excuseElements(when);
+
+  excuseP.innerHTML = newExcuse;
 };
